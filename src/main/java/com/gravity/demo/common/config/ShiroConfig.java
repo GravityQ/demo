c97ccc7b8855d2ea5a67d68fb09f8bb06d75a9b7
qijunlin <qijunlin@huobi.com>
2019-08-06 19:49:29 +0800

    初始化项目

---
+++ src/main/java/com/gravity/demo/common/config/ShiroConfig.java
@@ -0,0 +1,14 @@
+package com.gravity.demo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * shiro配置类
+ *
+ * @author qijunlin
+ * @date 2019-08-06 19:38
+ */
+@Configuration
+public class ShiroConfig {
+
+}
